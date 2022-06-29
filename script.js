2ae134c5393533dee3fdf183449f9434a9a79424
RobertaRizzuto <rtarizzuto@gmail.com>
2022-06-29 15:59:15 +0200

    fix:comment

--- script.js
+++ script.js
@@ -6,40 +6,7 @@ const addSerieBtnEl = q(".add-serie");
 const removeSerieBtnEl = q(".remove-serie");
 const deleteInputEl = q(".delete");
 
-// fetch(BASE_URL)
-//   .then(res => res.json())
-//   .then(data => data.map(serie => {
-//     const imgOrPlaceholder = serie.poster || 'https://picsum.photos/200/300';
 
-//     try {
-//       // if (!serie.description) {
-//       //   throw new Error('La serie non contiene una descrizione');
-//       // }
-//       createCard(document.body, imgOrPlaceholder, serie.title, serie.year);
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }))
-
-// addSerieBtnEl.addEventListener('click', () => {
-//   fetch(BASE_URL, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   }).then(() => location.reload())
-// })
-
-// const id = prompt('Inserisci id: ');
-// // DELETE - http
-// // fetch(BASE_URL+'/'+id, {
-// fetch(`${BASE_URL}/${id}`, {
-//   method: 'DELETE',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-// }).then(() => {})
 
 // ------------ il nuovo GET tramite funzione async/await, il vecchio sta sopra commentato a riga 17
 GET(BASE_URL).then((data) => {
@@ -79,13 +46,4 @@ deleteInputEl.addEventListener("input", (eventInput) => {
     DELETE(BASE_URL, eventInput.target.value).then(() => location.reload());
   });
 });
-
-// # Esercizio
-// Sulla base della lezione del giorno, aggiungere una form che permetta all'utente finale di fare una chiamata POST al server. Questo permette di aggiungere una nuova serie tv all'indirizzo: `https://edgemony-backend.herokuapp.com/series`
-
-// I passi da eseguire:
-// - Rimuovere la logica relativa al bottono Aggiunge serie
-// - Creare una form che includa 3 inputs, una per il titolo, una per imgUrl e una per anno (di produzione della serie)
-// - Creare un bottone al cui click i valori delle tre input sopra vengano spediti come parte del `body` utile alla chiamata POST
-// - Inoltre al click del bottone di sopra riaggiornare la pagina
 
